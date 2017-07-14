092d64dca9f60f8d35bda10b067857f1206a7ea6
Caitlin Quon <caitlin_quon@hotmail.com>
2017-07-13 18:58:23 -0700

    added video to homepage

--- styles/js/app.js
+++ styles/js/app.js
@@ -286,14 +286,14 @@ $( document ).ready(function() {
 
 });
 //Typed
-$(".element").each(function(){
-    var $this = $(this);
-    $this.typed({
-    strings: $this.attr('data-elements').split(','),
-    typeSpeed: 100, // typing speed
-    backDelay: 3000 // pause before backspacing
-    });
-});
+// $(".element").each(function(){
+//     var $this = $(this);
+//     $this.typed({
+//     strings: $this.attr('data-elements').split(','),
+//     typeSpeed: 100, // typing speed
+//     backDelay: 3000 // pause before backspacing
+//     });
+// });
 
 /* --------------------------------------------
     Video Script 
