2cba23ba42c130573e3d8da28824cfd883803072
Nombiezinja <tizh8835@gmail.com>
2017-07-15 13:29:32 -0700

    Adds seed files

--- server/db/seeds/a_users.js
+++ server/db/seeds/a_users.js
@@ -1,7 +1,6 @@
 const bcrypt = require("bcrypt");
 
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
   return knex('users').del()
     .then(function () {
       return Promise.all([
