b9faaaa508af3ff843a4e769a21b48a2f69ae641
Caitlin Quon <caitlin_quon@hotmail.com>
2017-07-16 16:59:57 -0700

    added some queries and configured some routes

--- server/routes/pet.js
+++ server/routes/pet.js
@@ -4,7 +4,8 @@ const express = require('express');
 const router  = express.Router();
 
 module.exports = (dbHelper) => {
-   router.get("/pet/new", (req, res)=>{
+  router.get("/pet/new", (req, res)=>{
+
     res.render("pet_register");
   });
 
@@ -14,7 +15,10 @@ module.exports = (dbHelper) => {
 
   router.post("/pet/new", (req, res) => {
     console.log(req.body);
+    console.log(req.session.user_id);
+    dbHelper.savePet(req.body, req.session.user_id).then((results) => {
     res.redirect("/pet");//to do: insert pet profile to the database and redirect to /pet/id
+    })
   })
 
  
