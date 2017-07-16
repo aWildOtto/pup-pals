b96017420e36e0d8c03d2a74ffdc6a1ac462ccbc
Nombiezinja <tizh8835@gmail.com>
2017-07-15 20:33:15 -0700

    Adds event_restrictions table and relevant column as well as seeds

--- server/db/seeds/b_events.js
+++ server/db/seeds/b_events.js
@@ -10,16 +10,18 @@ exports.seed = function(knex, Promise) {
           description: 'Bring your pups to the beach! All pups welcome.',
           location: 'Sunset Beach Park',
           event_time: new Date('2017-07-23 19:48:31.893241-07'),
-          open_status: true
+          open_status: true,
+          restriction: false
         }),
         knex('events').insert({
           id: 32,
           creator_user_id: '1',
           title: 'Small pups playdate',
-          description: 'Playdate at the dog park, small pups only please!',
+          description: 'Playdate at the dog park',
           location: 'Burnaby Heights Off-leash Park',
           event_time: '2017-06-05 19:48:31.893241-07' ,
-          open_status: true
+          open_status: true,
+          restriction: true
         })
       ])
     });
