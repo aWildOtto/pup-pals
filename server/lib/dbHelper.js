ae03cf746cef8bc5659ee219c335b08c345e2333
Nombiezinja <tizh8835@gmail.com>
2017-07-18 23:12:41 -0700

    Adds fix for neutered not being stored correctly upon pet creation

--- server/lib/dbHelper.js
+++ server/lib/dbHelper.js
@@ -141,7 +141,7 @@ module.exports = (knex) => {
         breed: pup.breed,
         size: pup.size,
         temperament: pup.temperament,
-        // neutered: "Yes"? true: false,
+        neutered: (pup.neutered === 'yes')? true: false,
         age: pup.age,
         avatar_url: pup.avatar_url,
         name: pup.name,
