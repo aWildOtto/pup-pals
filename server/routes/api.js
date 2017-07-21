4ab68b183411aad7027928d82069f9af0928b361
Nombiezinja <tizh8835@gmail.com>
2017-07-21 13:25:17 -0700

    rewrote ajax for owner statuses

--- server/routes/api.js
+++ server/routes/api.js
@@ -9,5 +9,33 @@ module.exports = (dbHelper) => {
       res.json(results);
     })
   });
+
+  router.get('/owner/:id', (req, res) => {
+    dbHelper.getUserStatus(req.params.id).then((results) => {
+      res.json(results);
+    })
+  });
+
+  router.post("/owner/:id", (req,res) => {
+    dbHelper.makeOwnerStatus(req.app.locals.user.id, req.body.text)
+      .then(() => {
+        res.redirect(`/owner/${req.app.locals.user.id}`)
+      })
+  });
+
+  router.get('/pet/:id', (req, res) => {
+    dbHelper.getPupStatuses(req.params.id).then((results) => {
+      res.json(results);
+    })
+  });
+
+  router.post("/pet/:id", (req, res) => {
+    dbHelper.makePupStatus(req.params.id, req.body.text)
+      .then(() => {
+        res.redirect(`/pet/${req.params.id}`)
+      })
+  })
+
   return router;
 }
+
