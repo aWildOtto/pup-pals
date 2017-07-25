028be89cfe982a1ce00c907504650e325b92af0a
donaldma <donald.ma@hotmail.ca>
2017-07-24 21:21:09 -0700

    temp commit

--- styles/js/calendar.js
+++ styles/js/calendar.js
@@ -33,20 +33,6 @@ $(document).ready(function () {
     });
 
   });
-
-  function loadEvents (){
-      $.ajax({
-        method: 'GET',
-        url: '/api/events',
-        data: $(this).serialize()
-      }).done(function(events){
-        events.map(function(event) {
-        console.log(event)
-    });
-    })
-  }
-  
-  loadEvents();
   /* initialize the calendar
   -----------------------------------------------------------------*/
 
@@ -113,54 +99,15 @@ $(document).ready(function () {
       }
 
     },
+
     events: [{
-        title: 'All Day Event',
-        start: new Date(y, m, 1)
-      },
-      {
-        id: 999,
-        title: 'Repeating Event',
-        start: new Date(y, m, d - 3, 16, 0),
-        allDay: false,
-        className: 'info'
-      },
-      {
-        id: 999,
-        title: 'Repeating Event',
-        start: new Date(y, m, d + 4, 16, 0),
-        allDay: false,
-        className: 'info'
-      },
-      {
         title: 'Meeting',
         start: new Date(y, m, d, 10, 30),
         allDay: false,
-        className: 'important'
+        className: 'info',
+        url: 'http://google.com/',        
       },
-      {
-        title: 'Lunch',
-        start: new Date(y, m, d, 12, 0),
-        end: new Date(y, m, d, 14, 0),
-        allDay: false,
-        className: 'important'
-      },
-      {
-        title: 'Birthday Party',
-        start: new Date(y, m, d + 1, 19, 0),
-        end: new Date(y, m, d + 1, 22, 30),
-        allDay: false,
-      },
-      {
-        title: 'Click for Google',
-        start: new Date(y, m, 28),
-        end: new Date(y, m, 29),
-        url: 'http://google.com/',
-        className: 'success'
-      }
     ],
     
   });
-
-
-    console.log(eventObject)
 });
